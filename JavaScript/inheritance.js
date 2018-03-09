e8755421092097468498be08b7768de8131a3202
simon.ward-jones <simon.ward-jones@beamly.com>
2018-03-09 12:29:08 +0000

    adding more explanation to prototypes

--- JavaScript/inheritance.js
+++ JavaScript/inheritance.js
@@ -16,7 +16,7 @@ function Dog(age){
 	Animal.call(this,age,'dog')
 	// call the animal function with given context
 }
-this would now create an aniaml with species dog
+// this would now create an aniaml with species dog
 
 // Now lets use and example with prototype
 function Animal(age,species,sound){
@@ -37,3 +37,18 @@ Dog.prototype = Object.create(Animal.prototype)
 Dog.prototype.constructor = Dog
 // This fixes the constructor being wrong
 
+
+// Really it comes down to two things
+// - fix the prototype
+// - fix the constructor
+
+// question why not use
+// Dog.prototype = Animal.prototype??
+// because then they are the same object !!!
+// and if we add a dog method to it all animals will have it!
+
+// why not use 
+// Dog.prototype = new Animal()
+// well if you look in the prototype of dog it includes all the instance specific
+// variables again but they are undefined as we didnt pass them to 
+// the Animal constructor. Just messy and could create strange bug
