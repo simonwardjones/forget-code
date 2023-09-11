134b12aa60f42f3ab1fa4673a93db2464f5afa18
simonwardjones <simon.ward-jones@deliveroo.co.uk>
2023-09-11 10:29:11 +0100

    Demonstrate multiple binaries in bin

---
+++ rust/exploration/modules/src/bin/one.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // Run this with `cargo run --bin one`
+    println!("Hello, world from binary create 1.");
+}
---
+++ rust/exploration/modules/src/bin/two.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // Run this with `cargo run --bin two`
+    println!("Hello, world from binary create 2.");
+}
